a49fd1771a8c4cee9589b67c7d291729ade44404
DamianAli <damian.ali33@gmail.com>
2017-06-18 14:04:55 -0700

    run against hbtn checks.

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -2,4 +2,4 @@
 ''' Print ascii value range of alphabet in lowercase'''
 for i in range(97, 123):
     if i != 101 and i != 113:
-        print('{}'.format(chr(i)), end='')
+        print(chr(i), end='')
